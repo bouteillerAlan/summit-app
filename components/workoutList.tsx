2d54bb9d8eeb7a296cfdf1db4200338c93a04e86
Alan A2N Bouteiller <a2n.dev@pm.me>
2023-06-27 13:54:29 +0200

    🚧 add workout card to the workout list component

--- components/workoutList.tsx
+++ components/workoutList.tsx
@@ -1,19 +1,21 @@
 import React, { type ReactElement, useEffect, useState } from 'react';
-import { Text, ScrollView } from 'native-base';
+import { ScrollView, VStack } from 'native-base';
 import type { AxsCallError, workout, workoutListProps } from '../types/interfaces';
 import Api from '../services/api';
 import { RefreshControl } from 'react-native';
 import { type AxiosResponse } from 'axios';
 import ErrorMessage from './errorMessage';
+import NoDataMessage from './noDataMessage';
+import WorkoutCard from './workoutCard';
 
 const WorkoutList = (props: workoutListProps): ReactElement => {
   const [apiError, setApiError] = useState<AxsCallError>({ message: '', inError: false });
   const [refreshing, setRefreshing] = useState<boolean>(false);
-  const [workoutList, setWorkoutList] = useState<workout[] | undefined>(undefined);
+  const [workoutList, setWorkoutList] = useState<workout[] | []>([]);
 
   useEffect(() => {
     getWorkoutForThePressedDate();
-  }, []);
+  }, [props.dayData]);
 
   /**
    * fetch the list of all the user workout for the pressed day (props.dayData.date)
@@ -29,11 +31,14 @@ const WorkoutList = (props: workoutListProps): ReactElement => {
   }
 
   return (
-    <ScrollView contentContainerStyle={{ paddingBottom: 125 }} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={getWorkoutForThePressedDate} />}>
+    // the paddingBottom value is unfortunately a magick number because I don't have a clue on how to make this ScrollView not going below the bottom navbar
+    // https://stackoverflow.com/questions/76509830/how-to-prevent-the-view-from-passing-under-the-navigation-expo-tabbar
+    <ScrollView contentContainerStyle={{ paddingBottom: 135, paddingRight: 10, paddingLeft: 10, paddingTop: 10 }} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={getWorkoutForThePressedDate} />}>
       <ErrorMessage condition={apiError.inError} text={apiError.message}/>
-      {workoutList?.map((z) => (
-        <Text>{JSON.stringify(z)}</Text>
-      ))}
+      <NoDataMessage condition={workoutList.length === 0} text={'No workout'}/>
+      <VStack space={4} alignItems={'center'}>
+        {workoutList.map((workout: workout) => <WorkoutCard key={workout.id} workout={workout}/>)}
+      </VStack>
     </ScrollView>
   );
 };
