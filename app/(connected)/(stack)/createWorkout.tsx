7d27377f8a7a99326911dcb71e83e655fcbb9a63
Alan A2N Bouteiller <a2n.dev@pm.me>
2023-07-10 14:14:36 +0200

    🚧 init the workout form

--- app/(connected)/(stack)/createWorkout.tsx
+++ app/(connected)/(stack)/createWorkout.tsx
@@ -1,11 +1,34 @@
-import React, { type ReactElement } from 'react';
-import { View, Text } from 'native-base';
+import React, { type ReactElement, useState } from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import type { dayData, WorkoutForm } from '../../../types/interfaces';
+import FormView from '../../../components/formView';
+import FormInput from '../../../components/formInput';
+import { FormDataValidation } from '../../../types/enum';
 
 const CreateWorkout = (): ReactElement => {
+  const localSearchParams: Partial<{ d: string }> = useLocalSearchParams<{ d: string }>();
+  const [formData, setFormData] = useState<WorkoutForm>({
+    trainingType: { value: '', onError: false, validation: [FormDataValidation.notEmpty] },
+    name: { value: 'my training', onError: false, validation: [FormDataValidation.notEmpty] },
+    date: { value: '', onError: false, validation: [FormDataValidation.notEmpty] },
+    formError: ''
+  });
+
+  /**
+   * read the query parameter 'd' from the url and transform it into a json if the param is not undefined
+   * @returns {dayData | undefined} dayData or undefined in function of the 'd' param
+   */
+  function rebuildDayDataJson(): dayData | undefined {
+    if (localSearchParams.d !== undefined) {
+      return JSON.parse(localSearchParams.d);
+    }
+    return undefined;
+  }
+
   return (
-    <View>
-      <Text>CreateWorkout</Text>
-    </View>
+    <FormView>
+      <FormInput type={'text'} isInvalid={true} placeholder={'workout name'} value={''} errorText={'oups'} onChangeText={() => { /**/ }}/>
+    </FormView>
   );
 };
 
